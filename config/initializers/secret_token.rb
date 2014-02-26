1956db3eac8828a7921039cb272ece1dd7e72447
Jwalanta Shrestha <jwalanta@gmail.com>
2014-02-25 21:59:38 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Khanchuwa::Application.config.secret_key_base = '91eaf2b6eb2c963ecea0615051f73a204263c0506dab712ca8b3c2d64f4ca80240fcfbe5f90084ee201f849082aae568e33b845e5b0dce43972856a6260aef0d'
